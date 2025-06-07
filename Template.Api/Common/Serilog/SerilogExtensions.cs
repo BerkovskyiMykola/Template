3c005c69a785aeaa543130d03383b09fc6930111
Mykola Berkovskyi <nikola04060206@gmail.com>
2025-06-07 03:22:16 +0300

    feat: init serilog configurations and add custom enricher

---
+++ Template.Api/Common/Serilog/SerilogExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using Serilog.Core;
+using Template.Api.Common.Serilog.Enrichers;
+
+namespace Template.Api.Common.Serilog;
+
+/// <summary>
+/// Provides extension methods for configuring Serilog in the application.
+/// </summary>
+internal static class SerilogExtensions
+{
+    /// <summary>
+    /// Configures and adds Serilog to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to which Serilog will be added.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null.</exception>  
+    public static void AddConfiguredSerilog(this IServiceCollection services)
+    {
+        services.AddSingleton<ILogEventEnricher, AppNameEnricher>();
+
+        services.AddSerilog((services, configuration) =>
+        {
+            configuration.ReadFrom.Configuration(services.GetRequiredService<IConfiguration>());
+            configuration.ReadFrom.Services(services);
+        });
+    }
+}
--- Template.Api/Program.cs
+++ Template.Api/Program.cs
@@ -1,4 +1,8 @@
+using Template.Api.Common.Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddConfiguredSerilog();
 
 var app = builder.Build();
 
