957f9ef009e96b1521972b4eb0bfcc27832b1d3f
Mykola Berkovskyi <nikola04060206@gmail.com>
2025-06-19 21:10:37 +0300

    feat: add monitoring and tracing

--- Template.Api/Program.cs
+++ Template.Api/Program.cs
@@ -1,20 +1,83 @@
+using System.Reflection;
 using System.Text;
 using Microsoft.AspNetCore.HttpLogging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 using Template.Api.Common.Logging;
 using Template.Api.Common.Middlewares;
 
+var applicationName = Assembly.GetEntryAssembly()?.GetName().Name 
+    ?? throw new ArgumentNullException("applicationName", "Unable to determine the application name from the executing assembly.");
+var serviceInstanceId = Ulid.NewUlid().ToString();
+
 var builder = WebApplication.CreateBuilder(args);
 
+var otelExporterOtlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+
+builder.Logging.ClearProviders();
+
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<CorrelationIdMiddleware>();
 
 builder.Services.AddSerilog((services, configuration) =>
 {
     configuration.ReadFrom.Configuration(services.GetRequiredService<IConfiguration>());
     configuration.ReadFrom.Services(services);
+
+    if (!string.IsNullOrWhiteSpace(otelExporterOtlpEndpoint))
+    {
+        configuration.WriteTo.OpenTelemetry(
+            endpoint: otelExporterOtlpEndpoint,
+            resourceAttributes: new Dictionary<string, object>
+            {
+                ["service.name"] = applicationName,
+                ["service.instance.id"] = serviceInstanceId
+            });
+    }
+
+    configuration.Enrich.WithProperty("ApplicationName", applicationName);
+    configuration.Enrich.WithProperty("ServiceInstanceId", serviceInstanceId);
 });
 
-builder.Services.AddTransient<CorrelationIdMiddleware>();
+if (!string.IsNullOrWhiteSpace(otelExporterOtlpEndpoint))
+{
+    builder.Services.AddOpenTelemetry()
+        .ConfigureResource(configuration =>
+        {
+            configuration
+                .AddService(
+                    serviceName: applicationName,
+                    serviceInstanceId: serviceInstanceId)
+                .AddEnvironmentVariableDetector()
+                .AddTelemetrySdk();
+        })
+        .WithMetrics(configuration =>
+        {
+            configuration
+                .AddRuntimeInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation();
+
+            configuration.AddOtlpExporter();
+        })
+        .WithTracing(configuration =>
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                configuration.SetSampler<AlwaysOnSampler>();
+            }
+
+            configuration
+                .AddAspNetCoreInstrumentation()
+                .AddHttpClientInstrumentation()
+                .AddEntityFrameworkCoreInstrumentation();
+
+            configuration.AddOtlpExporter();
+        });
+}
 
 var loggingFields = builder.Configuration.GetSection("HttpLogging:LoggingFields").Get<HttpLoggingFields>();
 var requestHeaders = builder.Configuration.GetSection("HttpLogging:RequestHeaders").Get<string[]>() ?? [];
